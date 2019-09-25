a47caa4133979ddb756eaac48e67d2c4e94cfb1f
Alan Vb <alan.vb@mail.ru>
2019-09-25 20:56:14 +0300

    Updated config file

--- config/semy-sms.php
+++ config/semy-sms.php
@@ -2,10 +2,23 @@
 
 return [
 
+    /**
+     * Token
+     * You can get it from: https://semysms.net/myprofile/usertoken
+     */
     'token' => env('SEMYSMS_TOKEN'),
 
+    /**
+     * Device ID
+     * This device will be used as default for your requests
+     * You can get Device ID on: https://semysms.net/myprofile/devices
+     */
     'device_id' => env('SEMYSMS_DEVICE_ID'),
 
+    /**
+     * Capture Incoming messages
+     * You set capture link on: https://semysms.net/myprofile/devices
+     */
     'catch_incoming' => false,
 
 ];
